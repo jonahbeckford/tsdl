411301007240c91f1cb524f3a1bb3be1f7b22f0f
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2025-06-30 14:58:33 -0700

    Create dune project

---
+++ support/cfg/discover.ml
@@ -0,0 +1,18 @@
+module C = Configurator.V1
+let () =
+  C.main ~name:"tsdl" (fun c ->
+    let default : C.Pkg_config.package_conf =
+      { libs   = ["-lSDL2"];
+        cflags = ["-I/usr/include"] }
+    in
+    let conf =
+      match C.Pkg_config.get c with
+      | None -> default
+      | Some pc ->
+        match C.Pkg_config.query pc ~package:"sdl2" with
+        | None -> default
+        | Some deps -> deps
+    in
+    C.Flags.write_sexp "cflags.sexp" conf.cflags;
+    C.Flags.write_sexp "c_library_flags.sexp" conf.libs;
+  )
